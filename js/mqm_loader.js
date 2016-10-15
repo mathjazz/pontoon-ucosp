160d270fe9272d3f2a793b27482a62be6fa2df65
Gordon Shieh <gshieh@sfu.ca>
2016-10-14 21:09:10 -0700

    Add check boxes to tree view

--- js/mqm_loader.js
+++ js/mqm_loader.js
@@ -2,16 +2,36 @@
 // Definition file to load the MQM selection dynamically via Javascript
 
 function addToTree(parent, treeItem) {
-    var list = document.createElement('ul');
-    var text = document.createElement('p');
-    text.textContent =  treeItem['name'];
-    list.appendChild(text);
-    for (var item in treeItem['children']) {
-        var child = document.createElement('li');
-        addToTree(child, treeItem['children'][item]);
-        list.appendChild(child);
-    }
-    parent.appendChild(list);
+
+    // Add a label if this isn't a leaf node, a checkbox otherwise
+    if (treeItem.hasOwnProperty('children')) {
+        var list = document.createElement('ul');
+        var title = document.createElement('p');
+        title.textContent = treeItem['name'];
+        list.appendChild(title);
+
+        for (var item in treeItem['children']) {
+            var child = document.createElement('li');
+            addToTree(child, treeItem['children'][item]);
+            list.appendChild(child);
+        }
+        parent.appendChild(list);
+    } else {
+        var idName = treeItem['name'].toLowerCase().replace(' ', '-');
+
+        var label = document.createElement('label');
+        label.setAttribute('for', idName);
+        label.textContent = treeItem['name'];
+
+        var checkbox = document.createElement('input');
+        checkbox.textContent = treeItem['name'];
+        checkbox.setAttribute('type', 'checkbox');
+        checkbox.setAttribute('class', 'issue-selection');
+        checkbox.setAttribute('id', idName);
+
+        parent.appendChild(label);
+        parent.appendChild(checkbox);
+    }    
 }
 
 window.addEventListener('load', function () {
